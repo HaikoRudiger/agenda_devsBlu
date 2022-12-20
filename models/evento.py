5bc7ef1aa20e9278ed3d53df4dddd2e1f1d4a3c9
FelipeWeiss1992 <felipeweiss92@gmail.com>
2022-12-20 18:27:03 -0300

    update class

--- models/evento.py
+++ models/evento.py
@@ -6,7 +6,7 @@ class Evento(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     id_usuario = db.Column(db.Integer, ForeignKey = (Usuario.id))
     data_evento = db.Column(db.Date, nullable = False)
-    titulo = db.Column(db.String(50), nullabel = False)
+    titulo = db.Column(db.String(50), nullable = False)
     descricao = db.Column(db.String(200), nullable = False)
     publico = db.Column(db.Boolean, default = False)
     ativo = db.Column(db.Boolean, default = True)
